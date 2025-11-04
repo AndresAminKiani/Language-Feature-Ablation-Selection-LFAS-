fab651710ebc12921521b0dd1185afe8ec261488
Andres Amin Kiani <136265091+AndresAminKiani@users.noreply.github.com>
2025-11-04 08:45:35 -0600

    Update Itiratively_Running_Functions_MainCode_1.R

--- RunElasticNetFunction_andSaveModelOutputs/Itiratively_Running_Functions_MainCode_1.R
+++ RunElasticNetFunction_andSaveModelOutputs/Itiratively_Running_Functions_MainCode_1.R
@@ -18,7 +18,7 @@ data <- find_file("Master9_Mtch_117x2_v13_with_PSs_1-1-18_Corrected634ID_zscore.
 ## How many iterations of models do we need/want to run?
 Num_of_iteration <- 200
 
-pathname = '/Users/susmusharma/Desktop/Classes_Records/First_Year_Paper/R_Codes/New_R_Codes_for_AllChildren//RunElasticNetFunction_andSaveModelOutputs'
+pathname = '***'
 ## Grabbing models output for the number of iterations using the model function
 Function_IterativelySaves_ModelOutput_3(data, Num_of_iteration, pathname)
 
